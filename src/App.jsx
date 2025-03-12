7ebe833bdf7db0696c652745d731ed80d1c72c09
fatma372 <fa1640@fayoum.edu.eg>
2025-03-12 23:56:06 +0200

    update footer path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/Header'
 import HomePage from './pages/HomePage/HomePage'
 import About from './pages/About/About'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Gallery from './pages/Gallery/Gallery'
 import FeedbackForm from './components/FeedbackForm'
 export default function App() {
